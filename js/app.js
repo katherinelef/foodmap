384bb665e14bdc1d0813e96766cb902591c3524b
katherinelef <katherine.lef@hotmail.com>
2017-12-22 12:47:59 -0500

    añadiendo avance splash

--- js/app.js
+++ js/app.js
@@ -1,38 +1,46 @@
+$(document).ready(function() {
+  $('h1').click(function() {
+    window.location.href = 'vista-1.html'
+
+  });
+});
+
+
+
 var rest1 = {
-    restaurant: 'tanta',
-    distrito: 'lima',
-    comida:'criolla'
-  };
+  restaurant: 'tanta',
+  distrito: 'lima',
+  comida: 'criolla'
+};
 
 var rest2 = {
-    restaurant: 'olimpico',
-    distrito: 'lima',
-    comida:'vegetariana'
-  };
+  restaurant: 'olimpico',
+  distrito: 'lima',
+  comida: 'vegetariana'
+};
 
 var rest3 = {
-    restaurant: 'rockys',
-    distrito: 'miraflores',
-    comida:'criolla'
-  };
+  restaurant: 'rockys',
+  distrito: 'miraflores',
+  comida: 'criolla'
+};
 
 var rest4 = {
-    restaurant: 'chenzu',
-    distrito: 'miraflores',
-    comida:'japonesa'
-  };
+  restaurant: 'chenzu',
+  distrito: 'miraflores',
+  comida: 'japonesa'
+};
 
 var rest5 = {
-    restaurant: 'mihan',
-    distrito: 'lince',
-    comida:'japonesa'
-  };
+  restaurant: 'mihan',
+  distrito: 'lince',
+  comida: 'japonesa'
+};
 
-var restaurantes = [rest1 , rest2, rest3, rest4, rest5];
+var restaurantes = [rest1, rest2, rest3, rest4, rest5];
 
-for(i=0; i<restaurantes.length; i++) {
-
-  if(restaurantes[i].distrito == 'lima') {
-    console.log(restaurantes[i].restaurant)
+for (i = 0; i < restaurantes.length; i++) {
+  if (restaurantes[i].distrito === 'lima') {
+    console.log(restaurantes[i].restaurant);
   }
 }
