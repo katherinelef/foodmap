2e372a11d3aa46a45a6429b434476f990c53717f
kathy <katherine.lef@hotmail.com>
2017-12-24 09:07:25 -0500

    añadiendo funcionalidad a filtros

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready(function(){
 
     $('h1').click(function() {
     window.location.href = 'vista-2.html'
@@ -8,7 +8,7 @@ $( "input" ).keyup(function() {
 
 var rest1 = {
   restaurant: 'tanta',
-  distrito: 'lima',
+  distrito: 'miraflores',
   comida: 'criolla'
 };
 
@@ -20,8 +20,8 @@ var rest2 = {
 
 var rest3 = {
   restaurant: 'rockys',
-  distrito: 'miraflores',
-  comida: 'criolla'
+  distrito: 'lima',
+  comida: 'rapida'
 };
 
 var rest4 = {
@@ -32,21 +32,59 @@ var rest4 = {
 
 var rest5 = {
   restaurant: 'mihan',
-  distrito: 'lince',
+  distrito: 'miraflores',
   comida: 'japonesa'
 };
 
-var restaurantes = [rest1, rest2, rest3, rest4, rest5];
+var rest6 = {
+  restaurant: 'casamama',
+  distrito: 'lince',
+  comida: 'criolla'
+};
+
+var rest7 = {
+  restaurant: 'el huerto',
+  distrito: 'lince',
+  comida: 'criolla'
+};
+
+var rest8 = {
+  restaurant: 'vegetales',
+  distrito: 'miraflores',
+  comida: 'vegetariana'
+};
+
+var rest9 = {
+  restaurant: 'kentucky',
+  distrito: 'lima',
+  comida: 'rapida'
+};
+
+var rest10 = {
+  restaurant: 'bembos',
+  distrito: 'lince',
+  comida: 'rapida'
+};
+
+var restaurantes = [rest1, rest2, rest3, rest4, rest5, rest6, rest7, rest8, rest9, rest10];
 
 for (i = 0; i < restaurantes.length; i++) {
   if (restaurantes[i].distrito === $("input").val()) {
-    $(".texto").append(restaurantes[i].restaurant);
-
+    $(".restaurantes").append("<p class='texto'>"+restaurantes[i].restaurant+"</p>");
+  }
+  else if($("input").val()==="") {
+    $(".texto").remove();
   }
 }
 
-
+if($("div.restaurantes p.texto").text()==="rockys") {
+  console.log("hola");
+}
 
 });
+
+if($(".texto").html()==="olimpico") {
+  $(".texto").addId("olimpico");
+}
 
 });
